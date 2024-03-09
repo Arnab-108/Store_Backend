89b8af2d884987851f8c40b2d984e5f5938d59bf
Arnab-108 <arnabadhikary007@gmail.com>
2024-03-09 18:35:41 +0530

    2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/files' , express.static('files'))
+app.use('/files' , express.static('tmp'))
 app.get("/" ,(req,res)=>{
     res.send("Homepage")
 })
